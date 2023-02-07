07791fc4da11e9953dd2dbe14c43faa3199831ad
yube01 <114811769+yube01@users.noreply.github.com>
2023-02-07 20:49:06 +0545

    scraping

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import axios from "axios";
 const port = 8888;
 import cheerio from "cheerio"
@@ -11,6 +11,35 @@ const app = express();
 
 app.get("/",(req,res)=>{
     res.json("Api activated")
+})
+
+
+
+//endpoints
+
+
+const post = []
+
+app.get("/sports",(req,res)=>{
+    axios.get("https://www.theguardian.com/uk/sport")
+    .then((response)=>{
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("sport")',html).each(()=>{
+            const title = $(this).text();
+            const url = $(this).attr("href")
+
+            post.push({
+                title,
+                url
+            })
+        }) ; res.json(post)
+        
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 })
 
 
